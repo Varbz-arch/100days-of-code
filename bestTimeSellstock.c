9af2afd3ac467ea141f48067baf174c430c0b9d4
Varbz-arch <archishavarbee@gmail.com>
2026-06-28 16:16:15 +0530

    Day2

---
+++ bestTimeSellstock.c
@@ -0,0 +1,46 @@
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+// Example 1:
+
+// Input: prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+#include <stdio.h>
+
+int maxProfit(int prices[], int n) {
+    int minPrice = prices[0];
+    int maxProfit = 0;
+
+    for (int i = 1; i < n; i++) {
+
+        // Found a cheaper buying price
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        }
+
+        // Calculate today's profit
+        int profit = prices[i] - minPrice;
+
+        // Update maximum profit
+        if (profit > maxProfit) {
+            maxProfit = profit;
+        }
+    }
+
+    return maxProfit;
+}
+
+int main() {
+    int prices[] = {7,1,5,3,6,4};
+    int n = sizeof(prices) / sizeof(prices[0]);
+
+    printf("%d", maxProfit(prices, n));
+
+    return 0;
+}
