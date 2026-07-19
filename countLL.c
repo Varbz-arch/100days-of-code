c6d7a8efd379a14b8cbdb46aa28b4d8cc2480a3c
Varbz-arch <archishavarbee@gmail.com>
2026-07-19 18:59:03 +0530

    Day22

--- countLL.c
+++ countLL.c
@@ -11,57 +11,99 @@
 // Output:
 // 10 20 30 40 50
 
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// // Structure for a node
+// struct Node
+// {
+//     int data;
+//     struct Node *next;
+// };
+
+// int main()
+// {
+//     int n;
+
+//     scanf("%d", &n);
+
+//     struct Node *head = NULL;
+//     struct Node *temp = NULL;
+//     struct Node *newNode = NULL;
+
+//     // Create the linked list
+//     for (int i = 0; i < n; i++)
+//     {
+//         newNode = (struct Node *)malloc(sizeof(struct Node));
+
+//         scanf("%d", &newNode->data);
+//         newNode->next = NULL;
+
+//         if (head == NULL)
+//         {
+//             head = newNode;
+//             temp = head;
+//         }
+//         else
+//         {
+//             temp->next = newNode;
+//             temp = newNode;
+//         }
+//     }
+
+//     // Traverse and count and print the linked list
+//     int count = 0;
+//     temp = head;
+
+//     while (temp != NULL)
+//     {
+//         count++;
+//         printf("%d \n", temp->data);
+//         temp = temp->next;
+//     }
+//     printf("%d \n", count);
+
+//     return 0;
+// }
+
 #include <stdio.h>
 #include <stdlib.h>
 
-// Structure for a node
-struct Node
-{
+struct Node {
     int data;
-    struct Node *next;
+    struct Node* next;
 };
 
-int main()
-{
-    int n;
-
-    scanf("%d", &n);
-
-    struct Node *head = NULL;
-    struct Node *temp = NULL;
-    struct Node *newNode = NULL;
-
-    // Create the linked list
-    for (int i = 0; i < n; i++)
-    {
-        newNode = (struct Node *)malloc(sizeof(struct Node));
-
-        scanf("%d", &newNode->data);
-        newNode->next = NULL;
-
-        if (head == NULL)
-        {
-            head = newNode;
-            temp = head;
-        }
-        else
-        {
-            temp->next = newNode;
-            temp = newNode;
-        }
-    }
-
-    // Traverse and count and print the linked list
+int countNodes(struct Node* head) {
     int count = 0;
-    temp = head;
-
-    while (temp != NULL)
-    {
+    while (head != NULL) {
         count++;
-        printf("%d \n", temp->data);
-        temp = temp->next;
+        head = head->next;
     }
-    printf("%d \n", count);
+    return count;
+}
 
+int main() {
+    int n, value;
+    scanf("%d", &n);
+    
+    struct Node* head = NULL;
+    struct Node* tail = NULL;
+    
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = value;
+        newNode->next = NULL;
+        
+        if (head == NULL) {
+            head = tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+    
+    printf("%d\n", countNodes(head));
     return 0;
 }
