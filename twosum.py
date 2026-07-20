7d2fc20c3da6424308c97361b122cc9472dac977
Archisha Borah <archishavarbee@gmail.com>
2026-07-20 16:14:54 +0530

    Day1

--- twosum.py
+++ twosum.py
@@ -80,3 +80,24 @@ print(twoSum([2,7,11,15], 9))
 
 #     return 0;
 # }
+
+
+
+
+# int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+#     static int result[2];
+    
+#     for (int i = 0; i < numsSize; i++) {
+#         for (int j = i + 1; j < numsSize; j++) {
+#             if (nums[i] + nums[j] == target) {
+#                 result[0] = i;
+#                 result[1] = j;
+#                 *returnSize = 2;
+#                 return result;
+#             }
+#         }
+#     }
+    
+#     *returnSize = 0;
+#     return NULL;
+# }
